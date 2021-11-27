8aee57e2efe890544e0cbb9522f200806a3b2664
Matej <matejchalachan@gmail.com>
2021-11-27 21:00:18 +0100

    product categories moved to product detail

--- src/product-detail.js
+++ src/product-detail.js
@@ -125,7 +125,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
                 .catch(err => console.error('Error loading product stock data: ',err))
         }
 
-//end of ajax
 
         //tato cast ma na starosti cekovanie na ktory produkt sa uzivatel pozeral
         if (code_trimmed) {
@@ -139,6 +138,29 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
                 }).catch(err => console.error('Get ip from ipify: ', err))
         }
+
+        const productID = document.getElementById('stars_main').dataset.productId;
+
+        fetch("URL FOR CATEGORIES HERE" + productID)
+            .then(res => res.json())
+            .then(data => {
+                if (data['success'] === true && data['categories']) {
+                    let catHtml = ' |  <a href="//www.mojadielna.sk/geko">Úvod</a> <span class="arrow">»</span> ';
+                    data['categories'].forEach(function (element) {
+                        catHtml += '<a href="' + element['full_url'] + '">' + element['catName'] + '</a> <span class="arrow">»</span> ';
+                    });
+                    const whereIEl = document.querySelector('#wherei p');
+
+                    if(whereIEl.querySelector('.active')) {
+                        catHtml += '<span class="active">' + whereIEl.querySelector('.active').innerHTML + '</span>';
+                    }
+
+                    if(whereIEl) {
+                        whereIEl.appendChild(catHtml)
+                    }
+                }
+            })
+            .catch(err => console.error('Error fetching product categories',err))
     }
 });
 
