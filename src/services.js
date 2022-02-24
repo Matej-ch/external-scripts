2fa3b088164bfa8a3ca721c18fef3a30300a560a
Matej <matejchalachan@gmail.com>
2022-02-24 15:38:29 +0100

    added simple modal window

--- src/services.js
+++ src/services.js
@@ -15,6 +15,28 @@ window.addEventListener('DOMContentLoaded', (event) => {
             referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
         }
 
+        function createServiceModal() {
+            const modal = document.createElement('div');
+            const modalContent = document.createElement('div');
+            const modalHeader = document.createElement('div');
+
+            modal.classList.add('js-ser-modal');
+            modal.setAttribute('id','myServiceModal');
+
+            modalContent.classList.add('js-modal-content');
+            modalHeader.classList.add('js-modal-header');
+
+            modalHeader.innerHTML = '<span class="js-s-close-modal css-s-close-modal">&times;</span><h2 class="js-service-header"></h2>';
+            modalContent.appendChild(modalHeader);
+
+            modalContent.innerHTML += `<div class="modal-body js-service-description" style="padding: 2px 16px;"></div>`;
+
+            modal.appendChild(modalContent);
+            document.querySelector('body').appendChild(modal);
+        }
+
+        createServiceModal();
+
         if (productCodeTrimmed) {
             fetch(serviceUrl)
                 .then(res => res.json())
@@ -32,9 +54,10 @@ window.addEventListener('DOMContentLoaded', (event) => {
                            const services = productData['additionalServices'];
 
                             services.forEach(service => {
-                                html += `<div style="padding-right: 10px;cursor: pointer"><img src="${service.icon}" alt='${service.name}' style="width: 60px;height: auto;"></div>`;
+                                html += `<div class="js-s-open-modal" style="padding-right: 10px;cursor: pointer">
+<img src="${service.icon}" alt='${service.name}' title="${service.name}" style="width: 60px;height: auto;"><input type="hidden" class="js-description-value" value="${service.description}"></div>`;
                             })
-                            servicesContainer.style.cssText = "display:flex;flex-direction:row;width:100%;";
+                            servicesContainer.style.cssText = "display:flex;flex-direction:row;width:100%;flex-wrap";
                             servicesContainer.innerHTML = html;
                         }
 
@@ -42,6 +65,26 @@ window.addEventListener('DOMContentLoaded', (event) => {
                     }
                 })
                 .catch(err => console.error('Error loading service data: ',err))
+        }
+
+        document.querySelector('body').addEventListener('click',e => {
+            if(e.target.classList.contains('.js-s-open-modal')) {
+                const modal = document.getElementById("myServiceModal");
+                modal.querySelector('.js-service-header').innerText = e.target.querySelector('img').getAttribute('alt');
+                modal.querySelector('.js-service-description').innerHTML = e.target.querySelector('.js-description-value').value;
+                modal.style.display = "block";
+            }
+
+            if(e.target.classList.contains('.js-s-close-modal')) {
+                document.getElementById("myServiceModal").style.display = "none";
+            }
+        })
+
+        // When the user clicks anywhere outside of the serviceModal, close it
+        window.onclick = function(event) {
+            if (event.target == document.getElementById("myServiceModal")) {
+                document.getElementById("myServiceModal").style.display = "none";
+            }
         }
     }
 })
