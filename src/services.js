1cdba8e8323d1a54098049de2f3d4432fb20d00b
Matej <matejchalachan@gmail.com>
2022-02-24 11:25:11 +0100

    added service url creation

--- src/services.js
+++ src/services.js
@@ -1,16 +1,21 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     if (document.querySelector('.product-detail-container')) {
-        const productServicesScriptTag = document.getElementById('product-detail-script')
+        const productServicesScriptTag = document.getElementById('product-service-script')
         const productDetailUrl = productServicesScriptTag.getAttribute("data-url");
 
         const productCode = document.querySelector("input[name='number']"); //hidden input
         const productCodeTrimmed = productCode.trim();
         const productCodeEncoded = encodeURIComponent(productCode.value);
 
+
         console.log('product code: ',productCode);
 
         console.log('productCodeTrimmed: ',productCodeTrimmed);
 
         console.log('productCodeEncoded: ',productCodeEncoded);
+
+        const serviceUrl = productDetailUrl + productCodeEncoded;
+
+        console.log(serviceUrl);
     }
 })
